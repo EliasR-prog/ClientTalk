883dce1f22242b1b99c099e7abad01484a208f4f
EliasR-prog <rabbat1965>
2020-11-19 15:15:22 +0000

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.sql.*;
 
 
 public class Main {
@@ -8,6 +11,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         makeGetRequest();
         makePostRequest();
+        SQLfunction();
+    }
+
+    private static void SQLfunction() {
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+        try {
+            // Registers the driver
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+        }
+
+        Connection conn= null;
+        try {
+            conn = DriverManager.getConnection(dbUrl, "postgres", "123");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            Statement s=conn.createStatement();
+            String sqlStr = "SELECT * FROM patients WHERE id>1;";
+            ResultSet rset=s.executeQuery(sqlStr);
+            while(rset.next()){
+                System.out.println(rset.getInt("id")+" "+ rset.getString("familyname"));
+            }
+            rset.close();
+            s.close();
+            conn.close();
+        }
+        catch (Exception e){
+        }
     }
 
     public static void makeGetRequest(){
@@ -55,14 +88,17 @@ public class Main {
     }
 
     public static void makePostRequest() throws MalformedURLException, IOException{
-        String message = "Hello servlet";
-        byte[] body = message.getBytes(StandardCharsets.UTF_8);
         URL myURL = new URL("http://localhost:8080/week5-6eliasrabbat/patients");
+        //String message = "Hello servlet";
+        Patient p = new Patient("Abdullah",69,"12345");
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(p);
+        byte[] body = jsonString.getBytes(StandardCharsets.UTF_8);
         HttpURLConnection conn = null;
         conn = (HttpURLConnection) myURL.openConnection();
         // Set up the header
         conn.setRequestMethod("POST");
-        conn.setRequestProperty("Accept", "text/html");
+        conn.setRequestProperty("Accept", "application/json");
         conn.setRequestProperty("charset", "utf-8");
         conn.setRequestProperty("Content-Length", Integer.toString(body.length));
         conn.setDoOutput(true);
